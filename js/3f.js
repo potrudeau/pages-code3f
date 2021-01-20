0e16183ebc89a7625b24d96fbff3554b2d454af3
Paul-Olivier Trudeau <po.trudeau@gmail.com>
2021-01-20 16:44:58 -0500

    MAJ

---
+++ js/3f.js
@@ -0,0 +1,43 @@
+function fixTeamname(name) {
+    switch (name) {
+        case "596079981566681088":
+            return "Fred Jean"
+            break;
+        case "484227143023194112":
+            return "Ian Boucher"
+            break;
+        case "590563686884614144":
+            return "JF Trudeau"
+            break;
+        case "474015432420880384":
+            return "Inouk Babin-R"
+            break;
+        case "335085800989425664":
+            return "Marc Drolet"
+            break;
+        case "591256971588448256":
+            return "Marty Desmarais"
+            break;
+        case "335454233417515008":
+            return "Samuel Comeau"
+            break;
+        case "590611424028434432":
+            return "Normand Bros."
+            break;
+        case "335249116051296256":
+            return "PO Trudeau"
+            break;
+        case "591271559289671680":
+            return "Éric Desgagné"
+            break;
+        case "591404060192591872":
+            return "MA Daigneault"
+            break;
+        case "331634294617292800":
+            return "Vincent Bessette"
+            break;
+        default:
+            return name;
+    }
+
+}
